f36f0258ab4ea389fd8c11216a240719fc20b644
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-03-02 13:21:01 +0100

    Creato header

---
+++ Frontend/web-portfolio-frontend/src/components/Header.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: "Header"
+
+}
+</script>
+
+<template>
+    <header>
+        <img src="../assets/logo.png" alt="">
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Contacts</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/_variables' as *;
+@use '../styles/partials/_mixins' as *;
+
+header {
+    @include d-flex(space-between, center);
+    height: 90px;
+    background-color: $primary-color;
+    padding: 0 20px;
+
+    ul {
+        @include d-flex(space-evenly, center);
+        gap: 10px;
+    }
+
+    li {
+        color: $secondary-color;
+        list-style: none;
+        font-size: 18px;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
+
+
+
+}
+</style>
