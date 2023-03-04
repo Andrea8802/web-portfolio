d0895597d1f711156c9ddd80eb6ff54b560da7a0
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-03-04 19:08:37 +0100

    Creato tasto per copiare email

--- Frontend/web-portfolio-frontend/src/components/HeaderProvvisorio.vue
+++ Frontend/web-portfolio-frontend/src/components/HeaderProvvisorio.vue
@@ -1,6 +1,22 @@
 <script>
 export default {
-    name: "HeaderProvvisorio"
+    name: "HeaderProvvisorio",
+    data() {
+        return {
+            emailCopied: false
+        }
+    },
+
+    methods: {
+        copyMail() {
+            navigator.clipboard.writeText("andreabonaventuradev@gmail.com");
+            this.emailCopied = true;
+            setTimeout(() => {
+                this.emailCopied = false;
+            }, 1500);
+
+        }
+    }
 
 }
 </script>
@@ -10,12 +26,28 @@ export default {
         <img src="../assets/logo.png" alt="">
         <nav>
             <ul>
-                <a href="https://github.com/Andrea8802" target="_blank">
-                    <fa icon="fa-brands fa-github" />
-                </a>
-                <a href="https://www.linkedin.com/in/andreabonaventuradev/" target="_blank">
-                    <fa icon="fa-brands fa-linkedin" />
-                </a>
+                <li>
+                    <a href="https://github.com/Andrea8802" target="_blank">
+                        <fa icon="fa-brands fa-github" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href="https://www.linkedin.com/in/andreabonaventuradev/" target="_blank">
+                        <fa icon="fa-brands fa-linkedin" />
+                    </a>
+                </li>
+
+                <li @click="copyMail" v-if="!emailCopied">
+                    <fa icon="fa-solid fa-envelope" />
+                </li>
+
+                <li v-else class="copied">
+                    <fa icon="fa-solid fa-clipboard-check" />
+                    <div class="text">
+                        Email Copiata!
+                    </div>
+                </li>
             </ul>
         </nav>
     </header>
@@ -31,12 +63,6 @@ header {
     background-color: $primary-color;
     padding: 0 80px;
 
-    a {
-        text-decoration: none;
-        color: $secondary-color;
-        font-size: 34px;
-    }
-
     img {
         height: 70%
     }
@@ -44,19 +70,32 @@ header {
     ul {
         @include d-flex(space-evenly, center);
         gap: 40px;
-    }
 
-    li {
-        color: $secondary-color;
-        list-style: none;
-        font-size: 24px;
-        font-weight: bold;
-        cursor: pointer;
-    }
+        li {
+            color: $secondary-color;
+            list-style: none;
+            font-size: 34px;
+            width: 30px;
+            font-weight: bold;
+            cursor: pointer;
 
-    @media screen and (max-width: 480px) {
-        header {
-            background-color: red;
+            a {
+                text-decoration: none;
+                color: $secondary-color;
+                font-size: 34px;
+            }
+        }
+
+        .copied {
+            position: relative;
+
+            .text {
+                position: absolute;
+                top: 70px;
+                left: -55px;
+                font-size: 20px;
+                width: 400px;
+            }
         }
     }
 
--- Frontend/web-portfolio-frontend/src/main.js
+++ Frontend/web-portfolio-frontend/src/main.js
@@ -5,8 +5,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fab)
+library.add(fab, fas)
 
 createApp(App).component('fa', FontAwesomeIcon).mount('#app')
